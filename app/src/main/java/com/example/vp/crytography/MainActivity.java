b12cf38bc592c662f8755bec88a9f772e582be2e
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-02-27 09:15:02 -0800

    more

--- app/src/main/java/com/example/vp/crytography/MainActivity.java
+++ app/src/main/java/com/example/vp/crytography/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.vp.crytography;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button[] clickme = new Button[1];
+    String input = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickme[0] =(Button)this.findViewById(R.id.button1);
+
     }
+
+    @Override
+    public void onClick(View v){
+        Button b = (Button)v;
+        //input = ((EditText)(this.findViewById(R.id.email_address))).getText();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
