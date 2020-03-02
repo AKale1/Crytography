ec2d54dbdf34324bb4d9d2811b37a7ef1a49c275
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-03-02 09:05:50 -0800

    UI

--- app/src/main/java/com/example/vp/crytography/MainActivity.java
+++ app/src/main/java/com/example/vp/crytography/MainActivity.java
@@ -1,34 +1,60 @@
 package com.example.vp.crytography;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.KeyEvent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Scanner;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    Button[] clickme = new Button[1];
+    EditText code;
     String input = "";
+    EditText num;
+    int inputNum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        clickme[0] =(Button)this.findViewById(R.id.button1);
+        addKeyListener();
 
     }
 
-    @Override
-    public void onClick(View v){
-        Button b = (Button)v;
-        //input = ((EditText)(this.findViewById(R.id.email_address))).getText();
-        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    public void addKeyListener(){
+        code = (EditText) findViewById(R.id.email_address);
+        code.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    input = code.getText().toString();
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        num = (EditText) findViewById(R.id.number);
+        num.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    //inputNum = Integer.parseInt(num.getText());
+                }
+
+                return false;
+            }
+        });
+
     }
+
+//    public String encryption(String input, int numRows){
+//
+//    }
 
 
 
